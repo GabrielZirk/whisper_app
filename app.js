502c5ddfc1b3113c2a565deeca6ca9aa47e45389
Gabriel Zirkovits <gabriel.zirkovits@edu.campus02.at>
2022-06-18 13:45:39 +0200

    Added package for environment variables

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
+require('dotenv').config(); //Require environment variable package as soon as possible
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-//----------------------------MongoDB/Mongoose setup
+//----------------------------MongoDB/Mongoose setup incl. encryption
 mongoose.connect("mongodb://localhost:27017/userDBSecrets");
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+//--- Define encryption before creating the model
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']}); //Encrypt only the password field
 
 const User = new mongoose.model("User", userSchema);
 
@@ -56,8 +62,8 @@ app.post("/register", (req, res) => {
 app.post("/login", (req, res) => {
     const loginMail = req.body.username;
     const loginPW = req.body.password;
-    User.findOne({email: loginMail},(err, foundUser) => {
-        if(err) {
+    User.findOne({ email: loginMail }, (err, foundUser) => {
+        if (err) {
             console.log(err);
         }
         else {
@@ -71,7 +77,8 @@ app.post("/login", (req, res) => {
                 res.send("OOOOOPS, wrong passord!");
             }
         }
-    })});
+    })
+});
 
 
 app.listen("3000", (err) => {
